eab4828bc2f1e49c3b0d0d1fafb650a4f7426c94
Kissa Leon Denis <61694317+Happy-Lama@users.noreply.github.com>
2024-04-05 03:46:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+# Define the URL of your Django server endpoint
+django_url = 'https://fyp-server-django.onrender.com/api/data/gsm/'
 
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    try:
+        # Get JSON data from the POST request
+        data = request.json
 
-if __name__ == "__main__":
-    app.run()
+        # Forward the received JSON data to the Django server
+        response = requests.post(django_url, json=data)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return jsonify({"message": "Data forwarded successfully to Django server"}), 200
+        else:
+            return jsonify({"error": f"Failed to forward data to Django server. Status code: {response.status_code}"}), response.status_code
+
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
